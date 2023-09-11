95e8c1472cd091ed1f2696a7cc68f336b371b8c6
Olanakelbesa <olanakelbesa@gmail.com>
2023-09-11 13:13:37 -0700

    ./102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: Always (success)
+ *
+ */
+int main(void)
+{
+int p, q;
+for (p = 0; p < 98; p++)
+{
+for (q = 0; q < 99; q++)
+{
+putchar((p / 10) + '0');
+putchar((p % 10) + '0');
+putchar(' ');
+putchar((q / 10) + '0');
+putchar((q % 10) + '0');
+if(p == 98 && q == 99)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
