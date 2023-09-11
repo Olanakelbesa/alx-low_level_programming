69b1bf24155282b761f6ddf432540dab0b90ca17
Olanakelbesa <olanakelbesa@gmail.com>
2023-09-11 10:29:39 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print lower and upper case
+ * Return: Always (success)
+ *
+ */
+int main(void)
+{
+char c;
+for(c = 'a'; c <= 'z'; c++)
+putchar(c);
+for(c = 'A'; c <= 'Z'; c++)
+putchar(c);
+putchar('\n');
+return 0;
+}
