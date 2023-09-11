df95b8d60f48acb21c5668c7cac7c38bfbd06f00
Olanakelbesa <olanakelbesa@gmail.com>
2023-09-11 11:09:57 -0700

    All your code should be in the main function

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - All your code should be in the main function
+ * Return: Always (Success)
+ *
+ */
+int main(void)
+{
+int n;
+for (n = 0; n < 10; n++)
+printf(n);
+putchar('\n');
+return (0);
+}
