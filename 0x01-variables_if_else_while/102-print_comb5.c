a0367653c6242287fccb841695a4f4339a41fd06
Olanakelbesa <olanakelbesa@gmail.com>
2023-09-11 13:17:17 -0700

    ./102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 int p, q;
-for (p = 0; p < 98; p++)
+for (p = 0; p <= 98; p++)
 {
-for (q = 0; q < 99; q++)
+for (q = 0; q <= 99; q++)
 {
 putchar((p / 10) + '0');
 putchar((p % 10) + '0');
 putchar(' ');
 putchar((q / 10) + '0');
 putchar((q % 10) + '0');
-if(p == 98 && q == 99)
+if (p == 98 && q == 99)
 continue;
 putchar(',');
 putchar(' ');
