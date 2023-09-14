bf57c8ffa5da39e4d410d71022ebda500b3fea9d
Olanakelbesa <olanakelbesa@gmail.com>
2023-09-14 09:51:55 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - check the code
  *
@@ -6,11 +7,7 @@
  */
 int main(void)
 {
-char c;
-for(c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
-_putchar('\n');
+print_alphabet();
 return (0);
 }
+
