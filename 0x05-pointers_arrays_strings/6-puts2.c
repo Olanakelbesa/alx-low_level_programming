7d66bebd897c7ae54e60bf676da81ca783bd0524
Olanakelbesa <olanakelbesa@gmail.com>
2023-09-19 10:48:54 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,12 @@
 #include "main.h"
+
 /**
-* puts2- Prints every other character of a string 
-* @str: The string to be treated 
-* Return: void
-*/
+ * puts2 - Prints every other character of a string
+ *
+ * @str: The string to be treated
+ *
+ * Return: void
+ */
 
 void puts2(char *str)
 {
